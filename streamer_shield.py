ed42aaa79a1c2e6b04a3cc008f6ae5730b187fcf
caesarakalaeii <caesarlp@gmx.de>
2023-10-23 20:05:34 +0200

    test

--- streamer_shield.py
+++ streamer_shield.py
@@ -75,7 +75,7 @@ class StreamerShield:
         
     
 if __name__ == "__main__":
-    ss = StreamerShield("attempt_4.h5","shield_vocab.json", 25)
+    ss = StreamerShield("attempt_4.h5","shield_vocab.json", 29)
     l = Logger(console_log=True)
     onehot = False
     
